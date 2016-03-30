cce1fbe1a1e13dbd81f5a7a0d38eaa317176d9df
Shane Tomlinson <stomlinson@mozilla.com>
2016-03-30 21:55:32 +0100

    Try to make the code intelligible
    * Try to make the variable names make sense.
    * Reduce so much indirection.
    * Remove TypedArrays

--- bloomfilter.js
+++ bloomfilter.js
@@ -3,66 +3,48 @@
   exports.fnv_1a = fnv_1a;
   exports.fnv_1a_b = fnv_1a_b;
 
-  var typedArrays = typeof ArrayBuffer !== "undefined";
-
   // Creates a new bloom filter.  If *m* is an array-like object, with a length
   // property, then the bloom filter is loaded with data from the array, where
   // each element is a 32-bit integer.  Otherwise, *m* should specify the
   // number of bits.  Note that *m* is rounded up to the nearest multiple of
   // 32.  *k* specifies the number of hashing functions.
-  function BloomFilter(m, k) {
-    var a;
-    if (typeof m !== "number") a = m, m = a.length * 32;
+  function BloomFilter(bits, hashingFunctions) {
+    var bucketCount = Math.ceil(bits / 32);
+    this.bits = bucketCount * 32;
+    this.hashingFunctions = hashingFunctions;
 
-    var n = Math.ceil(m / 32),
-        i = -1;
-    this.m = m = n * 32;
-    this.k = k;
-
-    if (typedArrays) {
-      var kbytes = 1 << Math.ceil(Math.log(Math.ceil(Math.log(m) / Math.LN2 / 8)) / Math.LN2),
-          array = kbytes === 1 ? Uint8Array : kbytes === 2 ? Uint16Array : Uint32Array,
-          kbuffer = new ArrayBuffer(kbytes * k),
-          buckets = this.buckets = new Int32Array(n);
-      if (a) while (++i < n) buckets[i] = a[i];
-      this._locations = new array(kbuffer);
-    } else {
-      var buckets = this.buckets = [];
-      if (a) while (++i < n) buckets[i] = a[i];
-      else while (++i < n) buckets[i] = 0;
-      this._locations = [];
-    }
-  }
+    this.buckets = new Array(bucketCount);
+    this.buckets.fill(0);
+}
 
   // See http://willwhim.wpengine.com/2011/09/03/producing-n-hash-functions-by-hashing-only-once/
   BloomFilter.prototype.locations = function(v) {
-    var k = this.k,
-        m = this.m,
-        r = this._locations,
+    var locations = new Array(this.hashingFunctions),
         a = fnv_1a(v),
         b = fnv_1a_b(a),
-        x = a % m;
-    for (var i = 0; i < k; ++i) {
-      r[i] = x < 0 ? (x + m) : x;
-      x = (x + b) % m;
+        x = a % this.bits;
+    for (var i = 0; i < locations.length; ++i) {
+      locations[i] = x < 0 ? (x + this.bits) : x;
+      x = (x + b) % this.bits;
     }
-    return r;
+    return locations;
   };
 
   BloomFilter.prototype.add = function(v) {
-    var l = this.locations(v + ""),
-        k = this.k,
-        buckets = this.buckets;
-    for (var i = 0; i < k; ++i) buckets[Math.floor(l[i] / 32)] |= 1 << (l[i] % 32);
+    var locations = this.locations(v + "");
+
+    for (var i = 0; i < locations.length; ++i) {
+      var bucket = locations[i];
+      this.buckets[Math.floor(bucket / 32)] |= 1 << (bucket % 32);
+    }
   };
 
   BloomFilter.prototype.test = function(v) {
-    var l = this.locations(v + ""),
-        k = this.k,
-        buckets = this.buckets;
-    for (var i = 0; i < k; ++i) {
-      var b = l[i];
-      if ((buckets[Math.floor(b / 32)] & (1 << (b % 32))) === 0) {
+    var locations = this.locations(v + "");
+
+    for (var i = 0; i < locations.length; ++i) {
+      var bucket = locations[i];
+      if ((this.buckets[Math.floor(bucket / 32)] & (1 << (bucket % 32))) === 0) {
         return false;
       }
     }
@@ -74,7 +56,7 @@
     var buckets = this.buckets,
         bits = 0;
     for (var i = 0, n = buckets.length; i < n; ++i) bits += popcnt(buckets[i]);
-    return -this.m * Math.log(1 - bits / this.m) / this.k;
+    return -this.bits * Math.log(1 - bits / this.bits) / this.hashingFunctions;
   };
 
   // http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
