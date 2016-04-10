05756ea2215391584486521f347d7fdffc3a8a50
Shane Tomlinson <stomlinson@mozilla.com>
2016-04-10 22:38:08 +0100

    add union and intersection

--- bloomfilter.js
+++ bloomfilter.js
@@ -3,27 +3,45 @@
   exports.fnv_1a = fnv_1a;
   exports.fnv_1a_b = fnv_1a_b;
 
-  // Creates a new bloom filter.  *bits* should specify the
-  // number of bits. *hashingFunctions* specifies the number of hashing functions.
+  /**
+   * An approachable explanation:
+   * http://www.michaelnielsen.org/ddi/why-bloom-filters-work-the-way-they-do/
+   *
+   * A great survey of the math behind filters, and their uses:
+   * https://www.eecs.harvard.edu/~michaelm/postscripts/im2005b.pdf
+   */
+
+  /**
+   * Create a new Bloom Filter
+   *
+   * @param {integer} bits - total number of bits in the storage table
+   * @param {integer} hashingFunctions - number of hashing functions to use
+   */
   function BloomFilter(bits, hashingFunctions) {
     this.hashingFunctions = hashingFunctions;
     var bytes = Math.ceil(bits / 32);
+    this.maxN = bytes * 32;
     this.buckets = new Uint32Array(bytes);
     this.buckets.fill(0);
   }
 
-  // See http://willwhim.wpengine.com/2011/09/03/producing-n-hash-functions-by-hashing-only-once/
   BloomFilter.prototype.locations = function(v) {
-    var locations = [],
-        bits = this.buckets.length * 32,
-        a = fnv_1a(v + ""),
-        b = fnv_1a_b(a),
-        x = a % bits;
+    var locations = [];
+
+    // Only two hashes are used to simulate n hashes.
+    // See http://willwhim.wpengine.com/2011/09/03/producing-n-hash-functions-by-hashing-only-once/
+    var a = fnv_1a(v + ""); // returns a 32bit signed int
+    var b = fnv_1a_b(a); // returns a 32bit signed int
 
     for (var i = 0; i < this.hashingFunctions; ++i) {
-      locations[i] = x < 0 ? (x + bits) : x;
-      x = (x + b) % bits;
+      // a + b * i will produce some possibly huge number, cut off at maxN
+      var g = (a + b * i) % this.maxN;
+      // % in the above calculation will convert the number to a 32bit
+      // signed number, which could be negative. All numbers correspond
+      // to an index in an array. Ensure the number is positive.
+      locations[i] = Math.abs(g);
     }
+
     return locations;
   };
 
@@ -41,48 +59,105 @@
     }, true);
   };
 
-  function toPosition(buckets, location) {
+  /**
+   * Create a new Bloomfilter that is the union of this filter
+   * plus another.
+   *
+   * @param {BloomFilter} filter
+   * @returns {BloomFilter}
+   */
+  BloomFilter.prototype.union = function(filter) {
+    if (this.maxN !== filter.maxN) {
+      throw new Error('filters must be the same size');
+    }
+
+    var unionFilter = new BloomFilter(this.maxN, this.hashingFunctions);
+    unionFilter.buckets = this.buckets.map(function (bucket, index) {
+      return bucket | filter.buckets[index];
+    });
+
+    return unionFilter;
+  };
+
+  BloomFilter.prototype.intersection = function(filter) {
+    if (this.maxN !== filter.maxN) {
+      throw new Error('filters must be the same size');
+    }
+    var unionFilter = new BloomFilter(this.maxN, this.hashingFunctions);
+    unionFilter.buckets = this.buckets.map(function (bucket, index) {
+      return bucket & filter.buckets[index];
+    });
+
+    return unionFilter;
+  };
+
+  function toPosition(location) {
     var bit = location % 32;
     var index = Math.floor(location / 32);
 
     return {
-      byte: buckets[index],
       index: index,
       mask: 1 << bit
     };
   }
 
   function setLocation(buckets, location) {
-    var position = toPosition(buckets, location);
-    buckets[position.index] = position.byte | position.mask;
+    var position = toPosition(location);
+    var newBucketValue = buckets[position.index] | position.mask;
+    buckets[position.index] = newBucketValue;
   }
 
   function isLocationSet(buckets, location) {
-    var position = toPosition(buckets, location);
-    return !! (position.byte & position.mask);
+    var position = toPosition(location);
+    var bucket = buckets[position.index];
+    return !! (bucket & position.mask);
   }
 
+  /**
+   * Fowler/Noll/Vo hashing. See
+   * https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash.
+   * For offset basis, see
+   * http://www.isthe.com/chongo/tech/comp/fnv/index.html#FNV-param
+   *
+   * @param {string} string to hash
+   * @returns 32bit signed int
+   */
 
-  // Fowler/Noll/Vo hashing.
-  function fnv_1a(v) {
-    var a = 2166136261;
-    for (var i = 0, n = v.length; i < n; ++i) {
-      var c = v.charCodeAt(i),
-          d = c & 0xff00;
-      if (d) a = fnv_multiply(a ^ d >> 8);
-      a = fnv_multiply(a ^ c & 0xff);
+  //https://gist.githubusercontent.com/vaiorabbit/5657561/raw/8ba52a3e13ad1d0cbf7c18bb93919120f09a8f02/fnv32a.js
+  function fnv_1a(str)
+  {
+    var FNV1_32A_INIT = 0x811c9dc5;
+    var hval = FNV1_32A_INIT;
+    for ( var i = 0; i < str.length; ++i )
+    {
+      hval ^= str.charCodeAt(i);
+      hval = fnv_multiply(hval);
     }
-    return fnv_mix(a);
+    // ensure result is a 32 bit signed int
+    return hval >>> 0;
   }
 
-  // a * 16777619 mod 2**32
+  /**
+   * Multiply two FNV results
+   * a * 16777619 mod 2**32
+   *
+   * @returns 32bit signed int
+   */
   function fnv_multiply(a) {
-    return a + (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
+    var result = a + (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
+    // ensure result is a 32 bit signed int
+    return result >>> 0;
   }
 
-  // One additional iteration of FNV, given a hash.
+  /**
+   * One additional iteration of FNV, given a hash value.
+   *
+   * @param {32bit signed in}
+   * @returns 32bit signed int
+   */
   function fnv_1a_b(a) {
-    return fnv_mix(fnv_multiply(a));
+    var result = fnv_mix(fnv_multiply(a));
+    return result;
   }
 
   // See https://web.archive.org/web/20131019013225/http://home.comcast.net/~bretm/hash/6.html
@@ -92,6 +167,8 @@
     a += a << 3;
     a ^= a >>> 17;
     a += a << 5;
-    return a & 0xffffffff;
+
+    return a >>> 0;
   }
+
 })(typeof exports !== "undefined" ? exports : this);
--- test/bloomfilter-test.js
+++ test/bloomfilter-test.js
@@ -10,10 +10,51 @@ var suite = vows.describe("bloomfilter");
 
 var jabberwocky = "`Twas brillig, and the slithy toves\n  Did gyre and gimble in the wabe:\nAll mimsy were the borogoves,\n  And the mome raths outgrabe.\n\n\"Beware the Jabberwock, my son!\n  The jaws that bite, the claws that catch!\nBeware the Jubjub bird, and shun\n  The frumious Bandersnatch!\"\n\nHe took his vorpal sword in hand:\n  Long time the manxome foe he sought --\nSo rested he by the Tumtum tree,\n  And stood awhile in thought.\n\nAnd, as in uffish thought he stood,\n  The Jabberwock, with eyes of flame,\nCame whiffling through the tulgey wood,\n  And burbled as it came!\n\nOne, two! One, two! And through and through\n  The vorpal blade went snicker-snack!\nHe left it dead, and with its head\n  He went galumphing back.\n\n\"And, has thou slain the Jabberwock?\n  Come to my arms, my beamish boy!\nO frabjous day! Callooh! Callay!'\n  He chortled in his joy.\n\n`Twas brillig, and the slithy toves\n  Did gyre and gimble in the wabe;\nAll mimsy were the borogoves,\n  And the mome raths outgrabe.";
 
+
+var jabberWords = jabberwocky.split(/\s+/).map(function (word) {
+  var cleaned = word.replace(/[\.`"',;\s!-]/g, '');
+  return cleaned;
+}).filter(function (word) {
+  return word.length;
+});
+
+var BITS = 1000;
+var HASHING_FUNCTIONS = 5;
+
 suite.addBatch({
+  "locations": {
+    topic: function () {
+      var f = new BloomFilter(BITS, HASHING_FUNCTIONS);
+      var locations = f.locations(jabberwocky);
+      return locations;
+    },
+
+    "the correct number of values are returned": function (locations) {
+      assert.equal(locations.length, HASHING_FUNCTIONS);
+    },
+
+    "all values are >= 0": function (locations) {
+      locations.forEach(function (location) {
+        assert.ok(location >= 0);
+      });
+    },
+
+    "all values are unique": function (locations) {
+      var unique = locations.reduce(function (unique, value) {
+        if (unique.indexOf(value) === -1) {
+          unique.push(value);
+        }
+
+        return unique;
+      }, []);
+
+      assert.equal(unique.length, HASHING_FUNCTIONS);
+    }
+  },
+
   "bloom filter": {
     "basic": function() {
-      var f = new BloomFilter(1000, 4),
+      var f = new BloomFilter(BITS, HASHING_FUNCTIONS),
           n1 = "Bess",
           n2 = "Jane";
       f.add(n1);
@@ -21,7 +62,7 @@ suite.addBatch({
       assert.equal(f.test(n2), false);
     },
     "jabberwocky": function() {
-      var f = new BloomFilter(1000, 4),
+      var f = new BloomFilter(BITS, HASHING_FUNCTIONS),
           n1 = jabberwocky,
           n2 = jabberwocky + "\n";
       f.add(n1);
@@ -29,7 +70,7 @@ suite.addBatch({
       assert.equal(f.test(n2), false);
     },
     "basic uint32": function() {
-      var f = new BloomFilter(1000, 4),
+      var f = new BloomFilter(BITS, HASHING_FUNCTIONS),
           n1 = "\u0100",
           n2 = "\u0101",
           n3 = "\u0103";
@@ -44,10 +85,59 @@ suite.addBatch({
       assert.equal(f.test("wtf"), false);
     },
     "works with integer types": function() {
-      var f = new BloomFilter(1000, 4);
+      var f = new BloomFilter(BITS, HASHING_FUNCTIONS);
       f.add(1);
       assert.equal(f.test(1), true);
       assert.equal(f.test(2), false);
+    }
+  },
+
+  "union": {
+    topic: function () {
+      var copy = [].concat(jabberWords);
+      var firstHalf = copy.splice(0, jabberWords.length / 2);
+      var secondHalf = copy;
+
+      var f1 = new BloomFilter(BITS, HASHING_FUNCTIONS);
+      var f2 = new BloomFilter(BITS, HASHING_FUNCTIONS);
+
+      firstHalf.forEach(f1.add.bind(f1));
+      secondHalf.forEach(f2.add.bind(f2));
+
+      return f1.union(f2);
+    },
+
+    "contains all words from both sets": function (unionFilter) {
+      jabberWords.forEach(function (word) {
+        assert.isTrue(unionFilter.test(word));
+      });
+    },
+
+    "returns a new filter that's the union of both": function (unionFilter) {
+      assert.isFalse(unionFilter.test("asdf"));
+      assert.isFalse(unionFilter.test("brilligs"));
+    }
+  },
+
+  "intersection": {
+    topic: function () {
+      var copy = [].concat(jabberWords);
+      var firstHalf = copy.splice(0, jabberWords.length / 2);
+      var secondHalf = copy;
+
+      var f1 = new BloomFilter(BITS, HASHING_FUNCTIONS);
+      var f2 = new BloomFilter(BITS, HASHING_FUNCTIONS);
+
+      firstHalf.forEach(f1.add.bind(f1));
+      secondHalf.forEach(f2.add.bind(f2));
+
+      return f1.intersection(f2);
+    },
+
+    "returns a new filter that's the interesection of both": function (intersectionFilter) {
+      assert.isTrue(intersectionFilter.test("gimble"));
+      assert.isFalse(intersectionFilter.test("frumious"));
+      assert.isFalse(intersectionFilter.test("asdf"));
     }
   }
 });
